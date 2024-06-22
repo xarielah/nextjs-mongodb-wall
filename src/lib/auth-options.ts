a2400a6446dd7d9b9519971bb42b22be50d12ef1
Ariel <arielah1997@gmail.com>
2024-06-22 12:05:37 +0300

    fix: auth-options is not valid route

---
+++ src/lib/auth-options.ts
@@ -0,0 +1,34 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "./db";
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ account, profile }: any) {
+      if (account.provider === "google") {
+        return profile.email_verified;
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+    async session({ session, user }: any) {
+      session.user = user;
+      return session;
+    },
+  },
+} as any;
