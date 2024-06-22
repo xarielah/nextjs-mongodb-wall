149bb8c8e8d60ede054e9e924940402cdb20748f
Ariel <arielah1997@gmail.com>
2024-06-22 14:12:11 +0300

    fix: bad post id on delete

--- src/app/wall/[wallId]/page.tsx
+++ src/app/wall/[wallId]/page.tsx
@@ -3,7 +3,7 @@ export default function UserWallPage({ wallId }: { wallId: string }) {
   console.log("🚀 ~ UserWallPage ~ wallId:", wallId);
   return (
     <div className="container-bg text-center text-zinc-500">
-      <h1 className="text-4xl">Ariel's wall</h1>
+      <h1 className="text-4xl">Ariel&apos;s wall</h1>
       <p className="text-xl">This is a wall for Ariel</p>
     </div>
   );
